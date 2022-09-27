662bb3563ed7a41db914572558cb0f750bbdeb9f
Nuwan <nuwanchamp02@gmail.com>
2022-09-27 08:06:06 +0530

    Integrate Google OAuth2.0 > Added Auth router > package included : nodemon, passport,passport-google-oauth2, express-session,jsonwebtoken > Modified authenticator middleware; > Access token verification > user loggin verification

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var restaurant_controller = require('../controllers/restaurant_controller');
+var { verifyToken } = require('../middlewares/authenticator');
 
-router.get('/', restaurant_controller.get_all_restaurants);
-router.get('/:id', restaurant_controller.get_restaurant_by_id);
-router.post('/', restaurant_controller.create_restaurant);
-router.put('/:id', restaurant_controller.update_restaurant);
-router.delete('/:id', restaurant_controller.delete_restaurant);
+router.get('/', verifyToken, restaurant_controller.get_all_restaurants);
+router.get('/:id', verifyToken, restaurant_controller.get_restaurant_by_id);
+router.post('/', verifyToken, restaurant_controller.create_restaurant);
+router.put('/:id', verifyToken, restaurant_controller.update_restaurant);
+router.delete('/:id', verifyToken, restaurant_controller.delete_restaurant);
 
 
 module.exports = router;
