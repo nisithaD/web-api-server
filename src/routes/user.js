8614a9838db739335fb31159caeed8575146fc94
Nisitha Dayarathna <nisitha56190@gmail.com>
2022-09-29 10:02:10 +0530

    Minor change added

--- src/routes/user.js
+++ src/routes/user.js
@@ -39,8 +39,6 @@ router.delete('/:id/favourites/:fid', (req, res) => { });
 
 const { register, login, verify_token, get_user } = require('../controllers/user_controller');
 
-const router = require('express').Router();
-
 router.post('/register', register)
 router.get('/login', login)
 router.get('/user', verify_token, get_user)
