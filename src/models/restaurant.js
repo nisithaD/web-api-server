e95c3befed3a8d318707c8d5025f381d3d844d0f
rumesh98888 <rumeshbandara9888@gmail.com>
2022-09-29 10:08:01 +0530

    Changes added

--- src/models/restaurant.js
+++ src/models/restaurant.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
-const RestaurantSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
 
-})
+const RestaurantSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: false },
+    rating: { type: Number, required: false },
+    display_image: { type: String, required: false },
+});
+
+
+
 
 const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
 
