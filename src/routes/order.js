f3915c82adb141fde6860270f7fdc21ef2c2c036
Nisitha Dayarathna <nisitha56190@gmail.com>
2022-10-02 21:36:38 +0530

    Routes changes added

--- src/routes/order.js
+++ src/routes/order.js
@@ -6,13 +6,13 @@ var { verifyToken,isLoggedIn } = require('../middlewares/authenticator');
 var orders_controller = require('../controllers/orders_controller');
 const auth = require('../middlewares/authenticator');
 
-router.post('/',isLoggedIn,orders_controller.create_order);
-router.get('/',isLoggedIn,orders_controller.get_all_orders);
-router.get('/:id',isLoggedIn,orders_controller.get_order);
-router.put('/:id',isLoggedIn,orders_controller.update_order);
-router.delete('/:id',isLoggedIn,orders_controller.delete_order);
-router.get('/restaurant/:id',isLoggedIn,orders_controller.get_restaurant_orders);
-router.put('/mark_as_completed/:id',isLoggedIn,orders_controller.mark_as_completed);
+router.post('/',verifyToken,isLoggedIn,orders_controller.create_order);
+router.get('/',verifyToken,isLoggedIn,orders_controller.get_all_orders);
+router.get('/:id',verifyToken,isLoggedIn,orders_controller.get_order);
+router.put('/:id',verifyToken,isLoggedIn,orders_controller.update_order);
+router.delete('/:id',verifyToken,isLoggedIn,orders_controller.delete_order);
+router.get('/restaurant/:id',verifyToken,isLoggedIn,orders_controller.get_restaurant_orders);
+router.put('/mark_as_completed/:id',verifyToken,isLoggedIn,orders_controller.mark_as_completed);
 
 module.exports = router;
 
